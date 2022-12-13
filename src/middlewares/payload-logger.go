d1eb33d98622ecabd424c080b2247e4a714e616f
ylfyt <ayudi3521@gmail.com>
2022-12-13 15:36:44 +0700

    Requst Payload Logger

---
+++ src/middlewares/payload-logger.go
@@ -0,0 +1,40 @@
+package middlewares
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"go-auth/src/ctx"
+	"go-auth/src/dtos"
+	"io"
+	"net/http"
+)
+
+func PayloadLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqId := r.Context().Value(ctx.ReqIdCtxKey)
+		body, err := io.ReadAll(r.Body)
+		if err == nil {
+			r.Body.Close()
+			r.Body = io.NopCloser(bytes.NewBuffer(body))
+			fmt.Printf("[%s] REQUEST PAYLOAD : %s\n", reqId, string(body))
+
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		response := dtos.Response{
+			Status:  http.StatusInternalServerError,
+			Message: "Something wrong",
+			Success: false,
+			Data:    nil,
+		}
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(response.Status)
+		err = json.NewEncoder(w).Encode(response)
+		if err != nil {
+			fmt.Println("Failed to send response", err)
+		}
+		fmt.Printf("[%s] REQUEST FAILED with RESPONSE:%+v\n", reqId, response)
+	})
+}
