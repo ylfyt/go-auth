550f4e91da67f0daeb6b26e479be477c269fb727
ylfyt <ayudi3521@gmail.com>
2022-12-22 16:34:57 +0700

    USAGE

--- main.go
+++ main.go
@@ -3,35 +3,30 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"go-auth/src/config"
+	"go-auth/src/controllers"
 	"go-auth/src/dtos"
+	"go-auth/src/utils"
+	"io"
+	"net/http"
 	"reflect"
-	// "net/http"
-	// "reflect"
-	// "go-auth/src/config"
-	// "go-auth/src/controllers"
 )
 
-func updateProduct(id string, data dtos.CreateProduct) {
+func updateProduct(id string, data dtos.CreateProduct) dtos.Response {
 	fmt.Println("Id:", id)
 	fmt.Println("Data:", data)
+	return utils.GetSuccessResponse(data)
 }
 
-func main() {
-	var ref interface{} = updateProduct
-	refType := reflect.TypeOf(ref)
+func getCallParams(r *http.Request, refFunc interface{}) []reflect.Value {
+	refType := reflect.TypeOf(refFunc)
 	var argTypes []reflect.Type
 	for i := 0; i < refType.NumIn(); i++ {
 		argType := refType.In(i)
 		argTypes = append(argTypes, argType)
 	}
 
-	jsonString := `
-				{
-					"name" : "Keyboard",
-					"description": "Keyboard baru dari paris",
-					"price" : 10000
-				}
-			`
+	jsonString, _ := io.ReadAll(r.Body)
 	idx := 0
 	urlParams := []string{}
 	var callParams []reflect.Value
@@ -50,11 +45,24 @@ func main() {
 		_ = json.Unmarshal([]byte(jsonString), &temp)
 		callParams = append(callParams, reflect.ValueOf(temp).Elem())
 	}
-	// fmt.Printf("Data: %+v\n", callParams[1])
-	reflect.ValueOf(ref).Call(callParams)
 
-	// router := controllers.NewRouter()
+	return callParams
+}
 
-	// fmt.Println("Server is listening on port", config.LISTEN_PORT)
-	// http.ListenAndServe("127.0.0.1:"+config.LISTEN_PORT, router)
+func main() {
+	router := controllers.NewRouter()
+	router.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		params := getCallParams(r, updateProduct)
+		response := reflect.ValueOf(updateProduct).Call(params)[0].Interface().(dtos.Response)
+
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(response.Status)
+		err := json.NewEncoder(w).Encode(response)
+		if err != nil {
+			fmt.Println("Failed to send response", err)
+		}
+	}).Methods("POST")
+
+	fmt.Println("Server is listening on port", config.LISTEN_PORT)
+	http.ListenAndServe("127.0.0.1:"+config.LISTEN_PORT, router)
 }
