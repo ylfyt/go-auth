bb783ba17fd34d40cc5317e419360ddf87bde047
ylfyt <ayudi3521@gmail.com>
2022-12-24 23:57:55 +0700

    With Connection Pool

--- src/db/setup.go
+++ src/db/setup.go
@@ -3,32 +3,65 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/lib/pq"
 	"go-auth/src/config"
+
+	_ "github.com/lib/pq"
 )
 
-const MAX_DB_CONN = 50
+const maxConnection = 50
 
-var dbConnChan = make(chan *sql.DB, MAX_DB_CONN)
+var isPoolMode bool = false
+
+var dbConnChan = make(chan *sql.DB, maxConnection)
 
 func init() {
-	for i := 0; i < MAX_DB_CONN; i++ {
-		dbConnChan <- nil
+	if isPoolMode {
+		fmt.Println("Setting up db with connection pool")
+	} else {
+		fmt.Println("Setting up db without connection pool")
+	}
+	for i := 0; i < maxConnection; i++ {
+		if !isPoolMode {
+			dbConnChan <- nil
+			continue
+		}
+		temp, err := sql.Open("postgres", config.DB_CONNECTION)
+		if err != nil {
+			fmt.Println("Failed to open connection :", err)
+			dbConnChan <- nil
+			continue
+		}
+		err = temp.Ping()
+		if err != nil {
+			fmt.Println("Failed to open connection :", err)
+			dbConnChan <- nil
+			continue
+		}
+		dbConnChan <- temp
 	}
 }
 
 func BorrowDbConnection() (*sql.DB, error) {
 	conn := <-dbConnChan
 	// Connect to DB
+	if conn != nil {
+		err := conn.Ping()
+		if err == nil {
+			return conn, nil
+		}
+		dbConnChan <- conn
+		return nil, err
+	}
+
 	newDB, err := sql.Open("postgres", config.DB_CONNECTION)
 	if err != nil {
-		dbConnChan <- conn
+		dbConnChan <- nil
 		return nil, err
 	}
 
 	err = newDB.Ping()
 	if err != nil {
-		dbConnChan <- conn
+		dbConnChan <- nil
 		return nil, err
 	}
 	return newDB, nil
@@ -36,9 +69,14 @@ func BorrowDbConnection() (*sql.DB, error) {
 
 func ReturnDbConnection(dbConn *sql.DB) {
 	// Close connection
+	if isPoolMode {
+		dbConnChan <- dbConn
+		return
+	}
+
 	err := dbConn.Close()
 	if err != nil {
 		fmt.Println("Failed to close connection")
 	}
-	dbConnChan <- dbConn
+	dbConnChan <- nil
 }
